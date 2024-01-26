d926f252115d4ef92bfd285434ace87742d07d8b
Hp <malik.muhdfaisal@gmail.com>
2024-01-26 12:06:45 +0500

    Added delete and update apis

--- CRUD_Application/simple_crud_app/urls.py
+++ CRUD_Application/simple_crud_app/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import UserListCreateView
+from .views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('fetch_Create_Users/', UserListCreateView.as_view()),
+    path('Delete_Update_User/<int:pk>/', UserRetrieveUpdateDestroyView.as_view()),
 ]
--- CRUD_Application/simple_crud_app/views.py
+++ CRUD_Application/simple_crud_app/views.py
@@ -5,3 +5,7 @@ from .serializers import UserSerializer
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
